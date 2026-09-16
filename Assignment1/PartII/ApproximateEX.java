17346a81947a2492a79de2b31a713383e7480bf1
Jordan7980 <trugamer53@gmail.com>
2026-09-15 22:40:27 -0400

    assignment 1

--- Assignment1/PartII/ApproximateEX.java
+++ Assignment1/PartII/ApproximateEX.java
@@ -1,10 +1,21 @@
 public class ApproximateEX {
 
+
+    public static int approximateEX(double x, int maxN) {
+        int sum = 0;
+        for(int n = 0; n < maxN; n++) {
+            int temp = 1;
+            for (int i = 1; i <= n; ++i) {
+                temp *= i;
+            }
+            sum += (int) Math.pow(x, n) / temp;
+        }
+
+        return sum;
+    }
 	
 	public static void main(String[] args) {
-		for(int i = 0; i < args.length; ++i){
-
-		}
-
+        int num = approximateEX(10.0, 32);
+        System.out.println("Approximate: " + num);
 	}
 }
